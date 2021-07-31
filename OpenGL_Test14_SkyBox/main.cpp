77456b49a966369b0ec79f83ac819c5977a93b2c
LiangHaoXiang <1013568397@qq.com>
2021-07-31 10:48:43 +0800

    Initial Commit

---
+++ OpenGL_Test14_SkyBox/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL_Test14_SkyBox
+//
+//  Created by Haoxiang Liang on 2021/7/31.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
